1d3f36c4348c86dc715809bc29bad7fc8d741955
Carl Factora <cfactora93@gmail.com>
2017-03-07 15:17:19 -0500

    moved intro code here

--- website/javascript/index.js
+++ website/javascript/index.js
@@ -1,10 +1,62 @@
-$(function() {
+function evaluate(editor) {
+  let collector = pageCode()
+
+  compile(getAllSources(collector), function(res) {
+    var identifier = $(editor.container).data('identifier');
+    if (res.error) {
+      $('p.js-errors.' + identifier).html(res.error.contents[0].message)
+    } else {
+      var replaced = res.js.replace(/require\("[^"]*"\)/g, function(s) {
+        return"PS['" + s.substring(12, s.length - 2) + "']";
+      });
+      var wrapped =
+          [ 'window.module = {};',
+            '(function(module) {',
+            replaced,
+            '})(module);',
+            'module.exports.main && module.exports.main();',
+          ].join('\n');
+      eval(wrapped)
+    }
+  })
+}
+
+$(() => {
   console.log('oh hai!')
 
   $('.js-editor').each(function() {
     createEditor(this)
   });
-});
+    
+  $('.js-go').click((event) => {
+    var $target = $(event.target)
+    clearFeedbacks()
+    let snippet = pageCode()
+    getAllSources(snippet)
+    snippet('main = logShow $ ' + $('.js-console').val())
+    compile(snippet(), (result) => {
+      if (result.error) {
+        $('.js-errors.' + $target.data('identifier'))
+          .html(result.error.contents[0].message)
+      } else {
+        var replaced = result.js.replace(/require\("[^"]*"\)/g, function(s) {
+          return"PS['" + s.substring(12, s.length - 2) + "']";
+        });
+        var wrapped =
+            [ 'window.module = {};',
+              '(function(module) {',
+              replaced,
+              '})(module);',
+              'module.exports.main && module.exports.main();',
+            ].join('\n');
+        overrideConsole('.js-results.' + $target.data('identifier'))
+        eval(wrapped)
+        restoreConsole()
+      }
+    })
+  })
+})
+
 
 function pageCode() {
   let all = 'module Main where\n' +
